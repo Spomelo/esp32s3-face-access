e4681866f5021ba18dce382969f612db8a185b44
mengyang zhu <137191211+Spomelo@users.noreply.github.com>
2025-09-18 22:18:37 +0800

    feat: add utils skeleton

---
+++ utils.c
@@ -0,0 +1,9 @@
+#include "esp_log.h"
+#include "esp_err.h"
+
+static const char *TAG = "UTILS";
+
+// Placeholder for future utility functions
+void utils_hello(void) {
+    ESP_LOGD(TAG, "utils ready");
+}

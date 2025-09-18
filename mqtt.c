dd600d79ed347270511292d01f8bf714ac755f80
mengyang zhu <137191211+Spomelo@users.noreply.github.com>
2025-09-18 22:13:39 +0800

    feat: add mqtt skeleton

---
+++ mqtt.c
@@ -0,0 +1,10 @@
+#include "esp_log.h"
+#include "esp_err.h"
+
+static const char *TAG = "MQTT";
+
+esp_err_t mqtt_start(void) {
+    ESP_LOGI(TAG, "mqtt_start (skeleton)");
+    // TODO: Create and start mqtt_client using configs
+    return ESP_OK;
+}
